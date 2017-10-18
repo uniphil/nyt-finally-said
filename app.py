37563f75bce98b278585766f23f8fcd425ac4639
phil <uniphil@gmail.com>
2017-10-18 01:34:20 -0400

    heroku without crashing maybe

--- app.py
+++ app.py
@@ -1,21 +1,30 @@
 import os
 import codecs
-import sqlite3
+import time
+# import sqlite3
 from lib.bottle import route, run, abort, template
 
-conn = sqlite3.connect(':memory:')
-conn.execute('CREATE TABLE words (word text PRIMARY KEY, year integer, books integer)')
+# conn = sqlite3.connect(':memory:')
+# conn.execute('CREATE TABLE words (word text PRIMARY KEY, year integer, books integer)')
 
+# with codecs.open('./deduped.csv', encoding='utf8') as f:
+#     conn.executemany('INSERT INTO words VALUES (?,?,?)',
+#         (line.split() for line in f))
+
+words = {}
+
+t0 = time.time()
 with codecs.open('./deduped.csv', encoding='utf8') as f:
-    conn.executemany('INSERT INTO words VALUES (?,?,?)',
-        (line.split() for line in f))
+    for word, year, books in map(lambda l: l.split(), f):
+        words[word] = (year, books)
+print time.time() - t0, 'to import'
 
 @route('/<word>')
 def index(word):
-    row = conn.execute('SELECT year, books FROM words WHERE word=?', (word,)).fetchone()
-    if row is None:
+    # row = conn.execute('SELECT year, books FROM words WHERE word=?', (word,)).fetchone()
+    if word not in words:
         return abort(404, 'pff {word}\'s not a word'.format(word=word))
-    year, books = row
+    year, books = words[word]
     return {
         'word': word,
         'year': year,
