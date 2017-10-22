3f4089bdd2ccc6b2d1376a06716c49721189bef0
phil <uniphil@gmail.com>
2017-10-22 10:27:12 -0400

    simple scanning search

--- app.py
+++ app.py
@@ -1,63 +1,25 @@
 import os
-import codecs
-import time
-# import sqlite3
 from lib.bottle import route, run, abort, template
 
-# conn = sqlite3.connect(':memory:')
-# conn.execute('CREATE TABLE words (word text PRIMARY KEY, year integer, books integer)')
+@route('/<word>')
+def index(word):
+    if len(word) < 1:
+        abort(400, 'plze provide a word to look up')
 
-# with codecs.open('./deduped.csv', encoding='utf8') as f:
-#     conn.executemany('INSERT INTO words VALUES (?,?,?)',
-#         (line.split() for line in f))
+    word_with_space = '{} '.format(word)
+    with open('./normalized.csv') as f:
+        for line in f:
+            if not line.startswith(word_with_space):
+                continue
+            break
+        else:
+            abort(404, 'pff {word}\'s not a word'.format(word=word))
 
-class Trie(object):
-    __slots__: ('year', 'books', 'children')
+    _, raw_year, raw_books = line.split(' ')
+    return {
+        'word': word,
+        'year': int(raw_year),
+        'books': int(raw_books),
+    }
 
-    def add(self, chars, year, books):
-        if len(chars) == 0:
-            self.year = year
-            self.books = books
-            return
-        c, *rest = chars
-        if not hasattr(self, 'children'):
-            self.children = {}
-        if c not in self.children:
-            self.children[c] = Trie()
-        self.children[c].add(rest, year, books)
-
-    def get(self, chars):
-        if len(chars) == 0:
-            if hasattr(self, 'year'):
-                return self.year, self.books
-            return None
-        c, *rest = chars
-        if not hasattr(self, 'children'):
-            return None
-        if c not in self.children:
-            return None
-        return self.children[c].get(rest)
-
-
-trie = Trie()
-
-
-t0 = time.time()
-with codecs.open('./deduped.csv', encoding='utf8') as f:
-    for word, year, books in map(lambda l: l.split(), f):
-        trie.add(word, year, books)
-print(time.time() - t0, 'to import')
-
-# @route('/<word>')
-# def index(word):
-#     # row = conn.execute('SELECT year, books FROM words WHERE word=?', (word,)).fetchone()
-#     if word not in words:
-#         return abort(404, 'pff {word}\'s not a word'.format(word=word))
-#     year, books = words[word]
-#     return {
-#         'word': word,
-#         'year': year,
-#         'books': books,
-#     }
-
-# run(host='localhost', port=int(os.environ.get('PORT', 8080)))
+run(host='localhost', port=int(os.environ.get('PORT', 8080)))
