3cece4e4469afbd0892ec4f51abf4ff8da35af81
phil <uniphil@gmail.com>
2017-10-22 16:33:09 -0400

    listen not just on localhost (heroku woo)

--- app.py
+++ app.py
@@ -10,11 +10,10 @@ def index(word):
         abort(400, 'plze provide a word to look up')
     except lookup.NotFound:
         abort(404, 'pff {word}\'s not a word'.format(word=word))
-
     return {
         'word': word,
         'year': year,
         'books': books,
     }
 
-run(host='localhost', port=int(os.environ.get('PORT', 8080)))
+run(host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
