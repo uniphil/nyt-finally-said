4759af1dc0903de5c5b7421bb5e58644a97ddaad
phil <uniphil@gmail.com>
2018-04-03 12:31:32 -0400

    add more stuff

--- manifestor/generate.py
+++ manifestor/generate.py
@@ -1,14 +1,22 @@
 from json import load
 from random import shuffle
 
+print("manifesto:\n")
+
+with open('manifestor/intros.json') as f:
+    sentences = load(f)
+    shuffle(sentences)
+    print('{}\n'.format(' '.join(sentences[:3])))
+
 with open('manifestor/bullets.json') as f:
     bullets = load(f)
     shuffle(bullets)
-    bullets = bullets[:5]
+    for bullet in bullets[:5]:
+        print("* {}\n".format(bullet))
 
-print("manifesto:\n")
-
-for bullet in bullets:
-    print("* {}\n".format(bullet))
+with open('manifestor/conclusions.json') as f:
+    sentences = load(f)
+    shuffle(sentences)
+    print('{}\n'.format(' '.join(sentences[:2])))
 
 print("-- yael, rachel, riley, phil, 2018")
